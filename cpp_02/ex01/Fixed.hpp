4b99696e22e062336e3899ecea35cae20c50f768
Tanmay Pandya <tanmaypandya159@gmail.com>
2025-12-19 15:55:29 +0100

    Started working on ex01 of module 02

---
+++ cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,43 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+/*
+ * COPY CONSTRUCTOR : It takes reference to another object of same class
+ * 		      We use const because we basically copy from 'other'...
+ *
+ * ASSIGNMENT OPERATOR : 'operator' -> special function name.. it tells that whenever you see the'=' sign between two objects of same class., the run this funciton
+ *
+ * 		-> It returns a reference to the object itself (*this)....
+ * 		-> a=b=c... b=c happens first which returns b, then a =b..
+ * 		*/
+
+class Fixed {
+	private:
+		int _fixedPointNumber;
+		static const int _fractionalBits = 8;
+	
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+
+		int	getRawBits(void) const;
+		void	setRawBits(int const bit);
+		// CONSTRUCTOR: int -> fixed point	
+		Fixed(const int n);
+		// CONSTRUCTOR: float -> fixed point
+		Fixed(const float n);
+		// fixed point -> float
+		float toFloat(void) const;
+		// fixed point -> int
+		int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
