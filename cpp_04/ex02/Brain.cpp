fa5cc9983e0f4e8d03857ffaa29223c6e6e558ff
Tanmay <tanmaypandya159@gmail.com>
2026-02-18 18:04:27 +0100

    made it consistent

--- cpp_04/ex02/Brain.cpp
+++ cpp_04/ex02/Brain.cpp
@@ -1,19 +1,22 @@
 #include "Brain.hpp"
 #include <iostream>
 
-Brain::Brain() {
-    std::cout << "Brain constructor called" << std::endl;
+Brain::Brain()
+{
+    std::cout << "<Brain> constructor called" << std::endl;
 }
 
-Brain::Brain(const Brain &other) {
-    std::cout << "Brain copy constructor called" << std::endl;
+Brain::Brain(const Brain &other) 
+{
+    std::cout << "<Brain> copy constructor called" << std::endl;
     for (int i = 0; i < 100; ++i) {
         this->ideas[i] = other.ideas[i];
     }
 }
 
-Brain &Brain::operator=(const Brain &other) {
-    std::cout << "Brain assignment operator called" << std::endl;
+Brain &Brain::operator=(const Brain &other) 
+{
+    std::cout << "<Brain> assignment operator called" << std::endl;
     if (this != &other) {
         for (int i = 0; i < 100; ++i) {
             this->ideas[i] = other.ideas[i];
@@ -22,6 +25,7 @@ Brain &Brain::operator=(const Brain &other) {
     return *this;
 }
 
-Brain::~Brain() {
-    std::cout << "Brain destructor called" << std::endl;
+Brain::~Brain() 
+{
+    std::cout << "<Brain> destructor called" << std::endl;
 }
--- cpp_04/ex02/main.cpp
+++ cpp_04/ex02/main.cpp
@@ -7,7 +7,6 @@ int main() {
     const int num_animals = 4;
     Animal* animals[num_animals];
 
-    // Create half Dogs and half Cats
     for (int i = 0; i < num_animals / 2; ++i) {
         animals[i] = new Dog();
     }
@@ -15,27 +14,24 @@ int main() {
         animals[i] = new Cat();
     }
 
-    // Delete all animals
     for (int i = 0; i < num_animals; ++i) {
         delete animals[i];
     }
 
-    std::cout << "\n--- Deep Copy Test ---\n" << std::endl;
+    std::cout << "\ntest to make sure that copys are not shallow\n" << std::endl;
 
-    // Test deep copy for Dog
     std::cout << "-> Testing Dog copy" << std::endl;
     Dog* original_dog = new Dog();
     Dog* copy_dog = new Dog(*original_dog);
-    delete original_dog; // Delete original, copy should still be fine
-    copy_dog->makeSound();
+    delete original_dog;
+	copy_dog->makeSound();
     delete copy_dog;
 
     std::cout << "\n-> Testing Cat copy" << std::endl;
-    // Test deep copy for Cat
     Cat* original_cat = new Cat();
     Cat* copy_cat = new Cat();
     *copy_cat = *original_cat;
-    delete original_cat; // Delete original, copy should still be fine
+    delete original_cat;
     copy_cat->makeSound();
     delete copy_cat;
 
