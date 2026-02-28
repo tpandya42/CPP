1d2f242270df1e8d9b491cf12e689127b3952f93
Tanmay Pandya <tanmaypandya159@gmail.com>
2026-02-28 12:14:54 +0100

    Done with module 07 ig

--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -2,20 +2,18 @@
 #include <string>
 #include "iter.hpp"
 
-// Function to print an element (works for const and non-const)
 template< typename T >
 void printElement( T const & x ) {
   std::cout << x << " ";
 }
 
-// Function to increment an element (works for non-const)
 template< typename T >
 void addOne( T & x ) {
   x++;
 }
 
 int main() {
-  std::cout << "--- Test with mutable int array ---" << std::endl;
+  std::cout << "ROUND 1: NON CONST " << std::endl;
   int tab[] = { 0, 1, 2, 3, 4 };
   size_t tab_size = 5;
 
@@ -30,15 +28,16 @@ int main() {
   std::cout << std::endl << std::endl;
 
 
-  std::cout << "--- Test with const char array ---" << std::endl;
+  std::cout << "ROUND 2: CONST " << std::endl;
   const char c_tab[] = "Hello World!";
   size_t c_tab_size = 12;
 
   std::cout << "Printing const array: ";
   iter( c_tab, c_tab_size, printElement<char> ); // Must use const overload
   std::cout << std::endl;
-  // The following line would cause a compile error, which is correct!
-  // iter( c_tab, c_tab_size, addOne<const char> );
+
+  // following line should throw an compiler errorr!!!!!!!!!!!!!!!!!11!
+   //iter( c_tab, c_tab_size, addOne<const char> );
 
   return 0;
 }
